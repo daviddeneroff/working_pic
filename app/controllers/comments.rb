1090514acaa00511d38fdd602af9a14c847ea683
Jay <jhd@fastmail.net>
2015-02-09 06:07:43 -0500

    change call to 'delete' to destroy, use local var in in post route

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -2,16 +2,14 @@ get '/comment/:post_id' do
   erb :'comments/comment_form', locals: {post_id: params[:post_id]}
 end
 
-post "/comments/:post_id" do
-  puts "PARAMS #{params}"
-  puts "USER #{session[:user_id]}"
-  Comment.create(content: params[:content], post_id: params[:post_id], user_id: current_user.id)
-  redirect '/posts'
+post "/comments/:post_id" do |post_id|
+  Comment.create(content: params[:content], post_id: post_id, user_id: current_user.id)
+  redirect "/game/post/#{post_id}"
 end
 
 delete "/comment/:id" do |id|
   comment = Comment.find(id)
-  comment.delete
+  comment.destroy
   redirect '/posts'
 end
 
